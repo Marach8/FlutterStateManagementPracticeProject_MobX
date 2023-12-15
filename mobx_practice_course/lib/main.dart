88e71fed0481f836a8e35a1d77c866f8d289deb2
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2023-12-15 09:21:58 +0100

    added packages for the project

--- mobx_practice_course/lib/main.dart
+++ mobx_practice_course/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobx_practice_course/views/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.blue)
+      ),
+      home: const HomePage()
+    );
   }
 }
